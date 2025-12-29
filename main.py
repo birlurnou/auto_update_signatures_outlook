44916efb8862b45e7a19cd2ce87639db734d1b45
birlurnou <birlurnou@gmail.com>
2025-12-29 09:29:01 +0500

    Update banner htm

--- main.py
+++ main.py
@@ -201,13 +201,20 @@ style='font-size:10.0pt;font-family:"Arial",sans-serif'><o:p></o:p></span></p>''
             if banner_path.lower().endswith('.png') or banner_path.lower().endswith('.jpg'):
                 try:
                     with open(banner_path, 'rb'):
-                        banner_html = f'''<p class=MsoNormal style='text-align:justify;text-justify:inter-ideograph;
-line-height:120%;text-autospace:none'><b><span style='font-size:9.0pt;
-line-height:120%;font-family:"Arial",sans-serif;color:#151F6D'><o:p>&nbsp;</o:p></span></b></p>
-
-<p class=MsoNormal><a href="{banner_url}">
-<img border=0 width=779 height=136 src="{banner_path}" style="border:none;">
-</a><o:p></o:p></p>'''
+# old
+#                         banner_html = f'''<p class=MsoNormal style='text-align:justify;text-justify:inter-ideograph;
+# line-height:120%;text-autospace:none'><b><span style='font-size:9.0pt;
+# line-height:120%;font-family:"Arial",sans-serif;color:#151F6D'><o:p>&nbsp;</o:p></span></b></p>
+#
+# <p class=MsoNormal><a href="{banner_url}">
+# <img border=0 width=779 height=136 src="{banner_path}" style="border:none;">
+# </a><o:p></o:p></p>'''
+                        banner_html = f'''<p class=MsoNormal>
+<a href="{banner_url}">
+ <img border=0 width=779 height=136 src="{banner_path}" style="border:none; display:block;">
+</a>
+<o:p></o:p>
+</p>'''
                 except:
                     base, ext = os.path.splitext(banner_path)
 
